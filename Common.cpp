212f6d18cdedd49c9406b478da56fc892386a5bf
sjrsjz <139971734+sjrsjz@users.noreply.github.com>
2023-10-17 19:34:17 +0800

    Delete .git__ directory

--- Common.cpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Common.h"
-size_t max_(size_t A, size_t B) {
-	return A > B ? A : B;
-}
-size_t min_(size_t A, size_t B) {
-	return A < B ? A : B;
-}
---
+++ cpp/ByteArray.cpp
@@ -0,0 +1,2 @@
+#pragma once
+#include "../header/ByteArray.h"
